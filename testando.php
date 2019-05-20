ca7fac050c4e9063a6239caa6ccc6d1da3a942ff
Thilanguis <gabrielt2005@hotmail.com>
2019-05-20 18:03:42 -0300

    colocando tela de apresentação para teste

---
+++ testando.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/fumaca.css">
+</head>
+<a href="index.php">
+
+    <body>
+        <section>
+            <video style="margin-left: 10%;" src="video/smoke.mp4" autoplay muted></video>
+            <h1>
+                <span>D</span>
+                <span>I</span>
+                <span>E</span>
+                <span>T</span>
+                <span>P</span>
+                <span>R</span>
+                <span>O</span>
+            </h1>
+        </section>
+    </body>
+</a>
+
+</html>
