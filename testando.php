4c97547d83b1dc379a8f5c8a54c18dd81b7b273a
Thilanguis <gabrielt2005@hotmail.com>
2019-05-21 03:09:22 -0300

    terminando tela de apresentação

--- testando.php
+++ testando.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="css/fumaca.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
 </head>
 <a href="index.php">
 
@@ -19,8 +22,49 @@
                 <span>P</span>
                 <span>R</span>
                 <span>O</span>
+                <br><br>
+                <span>A</span>
+                <span>p</span>
+                <span>r</span>
+                <span>e</span>
+                <span>s</span>
+                <span>e</span>
+                <span>n</span>
+                <span>t</span>
+                <span>a</span>
+                <span>:</span>
+                <br><br><br>
+                <span><img style="width:70px; height: 70px;" id="brocolis" class="oi animated fadeInLeft" src="img/icons8-br%C3%B3colis-48.png" alt=""></span>
+                <span><img style="width:70px; height: 70px;" id="tomate" class="oi animated fadeInDown" src="img/icons8-tomate-48.png" alt=""></span>
+                <span><img style="width:70px; height: 70px;" id="pera" class="oi animated fadeInUp" src="img/icons8-p%C3%AAra-48.png" alt=""></span>
+                <span style="top: 50%;"><img style="width:70px; height: 70px;" id="mamao" class="oi animated fadeInRight" src="img/icons8-mam%C3%A3o-48.png" alt=""></span>
+
             </h1>
+            <span id="orvalho1"><img class="oi animated bounceInDown" src="img/folhas-2.png" id="orvalho" alt=""></span>
+            <span id="brasao"><img class="oi animated zoomInLeft" src="img/simbolo%20de%20nutri%C3%A7%C3%A3o-800x800.png" id="brasao1" alt=""></span>
+            <div>
+                <h2 class="oi animated fadeIn"><span>S</span>
+                    <span>o</span>
+                    <span>f</span>
+                    <span>t</span>
+                    <span>w</span>
+                    <span>a</span>
+                    <span>r</span>
+                    <span>e</span>&nbsp;
+                    <span>d</span>
+                    <span>e</span>&nbsp;
+                    <span>N</span>
+                    <span>u</span>
+                    <span>t</span>
+                    <span>r</span>
+                    <span>i</span>
+                    <span>ç</span>
+                    <span>ã</span>
+                    <span>o</span></h2>
+            </div>
         </section>
+
+
     </body>
 </a>
 
