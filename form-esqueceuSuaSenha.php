28f0d7b0c87ba6c19f5999dcf800a45d7cf93940
thilanguis <gabrielt2005@hotmail.com>
2019-05-24 13:56:10 -0300

    arrumando código de recuperar senha

--- form-esqueceuSuaSenha.php
+++ form-esqueceuSuaSenha.php
@@ -5,8 +5,8 @@ include_once 'conexao.php';
 //pega a variavel via post
 $email = $_POST['email'];
 //busca no banco o usuario com o email
-$sql = "select * from cliente where EMAIL = '".$email."';";
-echo $sql;
+$sql = "select nome, senha from cliente where email = '".$email."'";
+
 //passa os dados para dentro de query
 $result = mysqli_query($con, $sql);
 //conta quantos tem
@@ -15,16 +15,20 @@ $registro = mysqli_num_rows($result);
 if($registro == 1){
 //fazemos um while para coletarmos as outras informações do usuario
 //tais como o nome e a senha
-while($row = mysqli_fetch_array($sql)){
-                $nome = $row['nome'];
-                $senha = $row['senha'];
+while($row = mysqli_fetch_array($result)){
+                $nome = $row[0];
+                $senha = $row[1];
                 }
 
 //envia o email para a pessoa juntamente com seu nome e sua senha
 $msg="Olá $nome, você solicitou a recuperação de senha para o sistema xxxxxx\n \n";
 $msg.="Sua senha é: $senha";
 
-mail($email, "Recuperação de senha", "$msg");
+$headers =  'MIME-Version: 1.0' . "\r\n"; 
+$headers .= 'From: Gabriel Granado <gabrielt@2005@hotmail.com>' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+mail($email, "Recuperação de senha","testando" , $headers);
 
 //alerta que o email foi enviado e o redireciona para outra página
 echo"<script>alert('Senha enviada por e-mail, verifique sua caixa de mensagens ou sua caixa de spans.'),window.open('index.php','_self')</script>";
@@ -33,6 +37,5 @@ echo"<script>alert('Senha enviada por e-mail, verifique sua caixa de mensagens o
 else{
 //lhe informa que o seu e-mail não está cadastrado no banco de dados
 echo"<script>alert('E-mail não cadastrado em nosso sistema, caso não se lembre do email cadastrado, entre em contato conosco.'),window.open('index.php','_self')</script>";
-
 }
 ?>
